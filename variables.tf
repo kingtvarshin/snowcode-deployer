136a1a410d36b8bede15e602b81690211bad3c43
Vikrant singh <kingtvarshin1@gmail.com>
2025-08-04 03:40:26 +0530

    test completed for first extension deployment

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "snowflake_account_name" {
+  description = "The name of the Snowflake account."
+  type        = string
+
+}
+
+variable "snowflake_org_name" {
+  description = "The name of the Snowflake organization."
+  type        = string
+
+}
+
+variable "snowflake_user" {
+  description = "The Snowflake user."
+  type        = string
+
+}
+
+variable "snowflake_password" {
+  description = "The password for the Snowflake user."
+  type        = string
+
+}
